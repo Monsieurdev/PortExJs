af5ed5933870100fcf00733eb5fa2526485216a0
mameloga@yahoo.fr <mameloga@hotmail.fr>
2017-12-30 23:04:51 -0500

    add landing page

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 app.get('/', function(req, res){
-	res.send('This is the langind page');
+	res.render('landing');
 });
 
 app.listen(3000, () => console.log('Server is running!'))
